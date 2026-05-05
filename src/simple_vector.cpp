cff06ab3b4b850c6b858c00e2272ae9a43deffff
Oleksandr K. <kotenko.oleksan-25@stud.dhbw-ravensburg.de>
2026-05-06 00:46:34 +0200

    fix: small fixes

--- src/simple_vector.cpp
+++ src/simple_vector.cpp
@@ -29,58 +29,40 @@ SimpleVector& SimpleVector::setY(double y) {
     this->y = y;
     return *this;
 }
+
 /**
- * @brief       Calculates length of vector
- * @return      double          length of the vector
+ * Math. logic
  */
-double abs() const {
+double SimpleVector::abs() const {
     return std::hypot(this->x, this->y);
 }
+
 /**
- * @brief       Addition of two vectors
- * @param[in]   vec             Vector to be added
- * @return      SimpleVector    Vector sum
+ * Operator
  */
-SimpleVector operator+(const SimpleVector& vec) const {
+SimpleVector SimpleVector::operator+(const SimpleVector& vec) const {
     return SimpleVector((this->x + vec.x), (this->y + vec.y));
 }
-/**
- * @brief       In-place addition of another vector
- * @param[in]   vec     Vector to be added
- */
-void operator+=(const SimpleVector& vec) {
+
+void SimpleVector::operator+=(const SimpleVector& vec) {
     this->x += vec.x;
     this->y += vec.y;
 }
-/**
- * @brief       Subtraction of two vectors
- * @param[in]   vec             Vector to be subtracted
- * @return      SimpleVector    Vector difference
- */
-SimpleVector operator-(const SimpleVector& vec) const {
+
+SimpleVector SimpleVector::operator-(const SimpleVector& vec) const {
     return SimpleVector((this->x - vec.x), (this->y - vec.y));
 }
-/**
- * @brief       In-place subtraction of another vector
- * @param[in]   vec     Vector to be subtracted
- */
-void operator-=(const SimpleVector& vec) {
+
+void SimpleVector::operator-=(const SimpleVector& vec) {
     this->x -= vec.x;
     this->y -= vec.y;
 }
-/**
- * @brief       Scalar multiplication
- * @param[in]   scalar          Factor for multiplication
- * @return      SimpleVector    The scaled vector
- */
-SimpleVector operator*(double scalar) const {
+
+SimpleVector SimpleVector::operator*(double scalar) const {
     return SimpleVector((this->x * scalar), (this->y * scalar));
 }
-/**
- * @brief       In-place scalar multiplication
- * @param[in]   scalar  Factor for multiplication
- */
-void operator*=(double scalar) {
+
+void SimpleVector::operator*=(double scalar) {
     this->x *= scalar;
     this->y *= scalar;
 }
