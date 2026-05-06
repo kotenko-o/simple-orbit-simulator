07f67a02be9b702e5135fa7b412c2f8a09f3a1a8
Oleksandr K. <kotenko.oleksan-25@stud.dhbw-ravensburg.de>
2026-05-06 09:42:05 +0200

    feat: modify class architecture

--- include/objects.hpp
+++ include/objects.hpp
@@ -11,15 +11,16 @@ class SpaceObject {
         int id;
         SimpleVector position;
         double mass;
+        SimpleVector appliedForce;
     public:
-        SpaceObject(int id, double mass, SimpleVector pos) 
-            : id(id), position(pos), mass(mass) {}
+        SpaceObject(int id, double mass, SimpleVector pos, SimpleVector appliedForce) 
+            : id(id), position(pos), mass(mass), appliedForce(appliedForce) {}
         virtual ~SpaceObject() {}
         /**
-         * @brief       Updating the position in relation to the object
-         * @param[in]   object      The object
+         * @brief       Updating the position with the applied force
          */
-        virtual void recalculatePos(const SpaceObject& object) = 0;        
+        virtual void recalculatePos() = 0;
+        virtual void calculateAppliedForce(const SpaceObject& object) = 0;
 };
 
 /**
@@ -30,7 +31,8 @@ class SpaceObject {
 class FixedObject : public SpaceObject {
     public:
         using SpaceObject::SpaceObject;
-        virtual void recalculatePos(const SpaceObject& object) override {};
+        virtual void recalculatePos() override {};
+        virtual void calculateAppliedForce(const SpaceObject& object) override {};
 };
 
 /**
@@ -39,18 +41,21 @@ class FixedObject : public SpaceObject {
 class FreeObject : public SpaceObject {
     protected:
         SimpleVector acceleration;
+        SimpleVector velocity;
         /**
          * @brief Calculates affect on acceleration of one object
          * @param[in]   object      The object
          */
         void calculateAffect(const SpaceObject& object);
     public:
-        FreeObject(int id, double mass, SimpleVector pos) : SpaceObject(id, mass, pos), acceleration(0,0) {}
+        FreeObject(int id, double mass, SimpleVector pos, SimpleVector velocity) 
+            : SpaceObject(id, mass, pos, SimpleVector(0, 0)), acceleration(SimpleVector(0, 0)), velocity(velocity) {}
         /**
          * @brief Calculate the momental speed
          */
-        double calculateSpeed() const;
-        void virtual recalculatePos(const SpaceObject& object) override;
+        void recalculateVelocity();
+        void virtual recalculatePos() override;
+        virtual void calculateAppliedForce(const SpaceObject& object) override;
 };
 
 #endif
