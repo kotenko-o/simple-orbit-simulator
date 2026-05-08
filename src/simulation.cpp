3bf8757ce87d2e5888e7c1ff87327201ce724997
Oleksandr K. <73932270+kotenko-o@users.noreply.github.com>
2026-05-08 03:08:35 +0200

    feat: add simulation class

---
+++ src/simulation.cpp
@@ -0,0 +1,21 @@
+#include "simulation.hpp"
+
+void Simulation::calculateSystem() {
+    int objectsSize = this->objects.size();
+    for (int i = 0; i < objectsSize - 1; i++) {
+        for (int j = i + 1; j < objectsSize; j++) {
+                objects[i]->calculateAppliedForce(*objects[j]);
+                objects[j]->calculateAppliedForce(*objects[i]);
+            }
+    }
+    for (SpaceObject* obj : objects) {
+        obj->recalculatePos(this->dt);
+    }
+}
+
+void Simulation::nextStep() {
+    for (SpaceObject* obj : objects) {
+        obj->reset();
+    }
+    this->current_tick++;
+}
