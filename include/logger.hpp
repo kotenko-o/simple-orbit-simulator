7b70b31cbc9bfac21559a995b1e48574802de9af
Oleksandr K. <73932270+kotenko-o@users.noreply.github.com>
2026-05-08 09:19:38 +0200

    feat: add logging module

--- include/logger.hpp
+++ include/logger.hpp
@@ -5,23 +5,37 @@
 #include "objects.hpp"
 #include <iostream>
 #include <iomanip>
+#include <fstream> 
 
-namespace console {
-
-    void startLogTable() {
-        return;
+namespace logs {
+    void startLogTable(std::ostream& stream) {
+        stream << std::setw(7) << "Tick "
+                << "; " << std::setw(3) << "id " 
+                << "; " << std::setw(10) << "x-pos" << "; " << std::setw(10) << "y-pos"
+                << "; " << "mass;" << std::endl;
     }
-    void logFullObjectInfo(FixedObject* obj) {
-        std::cout << std::setw(7) << 0
+    void logFullObjectInfo(std::ostream& stream, FixedObject* obj, int tick) {
+        stream << std::setw(7) << tick
                 << "; " << std::setw(3) << obj->getId() 
                 << "; " << std::setw(10) << obj->getPosition().getX() << "; " << std::setw(10) << obj->getPosition().getY()
                 << "; " << obj->getMass() << std::endl;
     }
-    void logFullObjectInfo(FreeObject* obj, int tick) {
-        std::cout << std::setw(7) << tick 
+    void logFullObjectInfo(std::ostream& stream, FreeObject* obj, int tick) {
+        stream << std::setw(7) << tick 
                 << "; " << std::setw(3) << obj->getId() 
                 << "; " << std::setw(10) << obj->getPosition().getX() << "; " << std::setw(10) << obj->getPosition().getY()
                 << "; " << obj->getMass() << std::endl;
+    }
+}
+
+namespace csvLog {
+    const std::string DEFAULT_FILE_NAME = "simulation_result.csv";
+    std::fstream startLog() {
+        std::fstream myFile(DEFAULT_FILE_NAME, std::ios::out);
+        return std::move(myFile);
+    }
+    void closeLog(std::fstream& myFile) {
+        myFile.close();
     }
 }
 
