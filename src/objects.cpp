a2a078046e9fca39c8526c4be650bfb99fdf264d
Oleksandr K. <73932270+kotenko-o@users.noreply.github.com>
2026-05-08 08:24:46 +0200

    fix: distace calculation

--- src/objects.cpp
+++ src/objects.cpp
@@ -4,17 +4,20 @@
 
 void FreeObject::calculateAppliedForce(const SpaceObject& object) {
     SimpleVector distance = this->getPosition() - object.getPosition();
-    double force = physics::calculateGravitationalForce(this->getMass(), object.getMass(), distance.abs() == 0 ? distance.abs() : 1);
+    double r = distance.abs();
+    double dist = (r < 1e-9) ? 1 : r;
+    double force = physics::calculateGravitationalForce(this->getMass(), object.getMass(), dist);
     this->appliedForce += distance.getUnitVector() * force * (-1);
 }
 
 
 void FreeObject::recalculateVelocity(double dt) {
-    this->acceleration += this->appliedForce * (1.0 / this->mass);
+    this->acceleration = this->appliedForce * (1.0 / this->mass);
     this->velocity += this->acceleration * dt;
 }
 
 void FreeObject::recalculatePos(double dt) {
+    this->recalculateVelocity(dt);
     this->position += this->velocity * dt;
 }
 
