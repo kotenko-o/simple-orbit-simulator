8fbe708d479d4a5c83f82f142e98104508582d35
Oleksandr K. <73932270+kotenko-o@users.noreply.github.com>
2026-05-12 00:52:15 +0200

    docs: update doxygen

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -4,74 +4,86 @@
 #include "objects.hpp"
 
 /**
- * @class The class that contains the simulation loop and all the objects
+ * @class Simulation
+ * @brief Manages the lifecycle of simulations and space objects
  */
-
 class Simulation {
     private:
-        int id = 0;
+        int id = 0;                              // Counter for objects id's
         std::uint64_t current_tick = 0;
         std::uint64_t last_tick = 0;
+        // Container for all objects in simulation
         std::vector<std::unique_ptr<SpaceObject>> objects;
-        double dt;
+        double dt;                              // Timestep for calculations
     public:
         /**
          * @brief   Constructor
-         * @param[in]   lastTick    Tick up to which simulation should run
-         * @param[in]   dt          Factor for time
+         * @param[in]   lastTick    Number of ticks, simulation should do
+         * @param[in]   dt          Physical timestep
          */
         Simulation(std::uint64_t lastTick, double dt) 
             : last_tick(lastTick), dt(dt) {}
         /**
          * @brief   Destructor
          */
-        ~Simulation() {}
+        ~Simulation() = default;
         /**
-         * @brief   Add new object to a simulation
+         * @brief   Add an existing object to a simulation
          * @param[in]   obj     New object
          */
         void addObject(std::unique_ptr<SpaceObject> obj) {
             objects.push_back(std::move(obj));
         }
+
+        /* ------------------- 
+         *  Simulation logic 
+         * -------------------*/
         /**
-         * @brief   Remove an object from simulation
-         * @param[in]   obj     Pointer to object to remove
-         */
-        void removeObject(SpaceObject* obj);
-        /**
-         * @brief   Main loop for each tick
+         * @brief   Calculates "physics" for all objects
+         * Calculates the forces exerted by all objects on all other objects and 
+         * determines the positions of the free objects
          */
         void calculateSystem();
         /**
-         * @brief   Check for collisions and take action
+         * @brief   Check for collisions
+         * @todo    Update workaround for output
+         * @todo    Optimize algorithm O(n^2)
          */
         void checkCollisions() const;
         /**
-         * @brief   Tick change, "cleaning"
+         * @brief   Next step: Tick increment, coordinates reset
          */
         void nextStep();
         /**
-         * @brief   Getter for dt
-         * @return  double  dt
+         * @brief   Checks, if function should run
          */
+        bool running() const {
+            if (this->current_tick < this->last_tick) {
+                return true;
+            }
+            return false;
+        }
+        
+        /* ------------------- 
+         *  Getter Methods 
+         * -------------------*/
         double getDt() const {
             return this->dt;
         }
-        /**
-         * @brief   Getter for Current Tick
-         * @return  double  Curret Tick
-         */
-        int64_t getCurrentTick() const {
+        uint64_t getCurrentTick() const {
             return this->current_tick;
         }
-        /**
-         * @brief   Getter for last Tick
-         * @return  double  last Tick
-         */
-        int64_t getLastTick() const {
+        uint64_t getLastTick() const {
             return this->last_tick;
         }
 
+        /* ------------------- 
+         *  Fabric Methods 
+         * -------------------*/
+        /**
+         * @brief   Fabric method for creation of the free object
+         * @return  SpaceObject*    Pointer to the object als reference
+         */
         SpaceObject* createFreeObject(double mass, SimpleVector pos, SimpleVector velocity, std::unique_ptr<Hitbox> hb = nullptr) {
             auto new_object = std::make_unique<FreeObject>(id, mass, pos, velocity, std::move(hb));
             SpaceObject* ptr = new_object.get();
@@ -79,19 +91,15 @@ class Simulation {
             this->id++;
             return ptr;
         }
-
+        /**
+         * @brief   Fabric method for creation of the fixed object
+         * @return  SpaceObject*    Pointer to the object als reference
+         */
         SpaceObject* createFixedObject(double mass, SimpleVector pos, std::unique_ptr<Hitbox> hb = nullptr) {
             auto new_object = std::make_unique<FixedObject>(id, mass, pos, std::move(hb));
             SpaceObject* ptr = new_object.get();
             objects.push_back(std::move(new_object));
             this->id++;
             return ptr;
-        }
-
-        bool running() {
-            if (this->current_tick < this->last_tick) {
-                return true;
-            }
-            return false;
         }
 };
