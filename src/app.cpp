8a7fcbc3af4d4a13e1240ff4546e81239615ac69
Oleksandr K. <73932270+kotenko-o@users.noreply.github.com>
2026-05-12 01:06:19 +0200

    feat: update app.cpp to new simulation

--- src/app.cpp
+++ src/app.cpp
@@ -13,19 +13,19 @@ int main() {
     double mMoon = 1.23e10;
     double mShip = 1.0;
 
-    Simulation sim(600000, 0.01);
+    Simulation sim(60000, 0.01);
     std::fstream file = csvLog::startLog();
 
-    sim.createFreeObject(mEarth, SimpleVector(0, 0), SimpleVector(0, -0.00513));
-    sim.createFreeObject(mMoon,  SimpleVector(384, 0),  SimpleVector(0, 0.419));
+    SpaceObject* earth = sim.createFreeObject(mEarth, SimpleVector(0, 0), SimpleVector(0, -0.00513));
+    SpaceObject* moon = sim.createFreeObject(mMoon,  SimpleVector(384, 0),  SimpleVector(0, 0.419));
 
     logs::startLogTable(file);
 
-    while (sim.getCurrentTick() < sim.getLastTick()) {
+    while (sim.running()) {
         sim.calculateSystem();
-        if (sim.getCurrentTick() % 5000 == 0) {
+        if (sim.getCurrentTick() % 10000 == 0) {
             logs::logFullObjectInfo(file, earth, sim.getCurrentTick());
-            logs::logFullObjectInfo(file, &moon, sim.getCurrentTick());
+            logs::logFullObjectInfo(file, moon, sim.getCurrentTick());
         }
         sim.checkCollisions();
         sim.nextStep();
