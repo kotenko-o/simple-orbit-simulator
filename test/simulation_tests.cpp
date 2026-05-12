745ae1bfdd1b31e164205fa3e011820cb99bb676
Oleksandr K. <73932270+kotenko-o@users.noreply.github.com>
2026-05-12 16:59:36 +0200

    feat: integrate Catch2

--- test/simulation_tests.cpp
+++ test/simulation_tests.cpp
@@ -1,9 +1,9 @@
+#include <catch2/catch_test_macros.hpp>
 #include <simulation.hpp>
 #include <objects.hpp>
-#include <test_framework.h>
 #include <simple_vector.hpp>
 
-void test_simple_two_body_movement() {
+TEST_CASE("two bodys movement", "[simulation]") {
     double mass = 1e12;
     Simulation sim(10, 0.0);
 
@@ -12,13 +12,6 @@ void test_simple_two_body_movement() {
 
     sim.calculateSystem();
 
-    N_ASSERT(p1->getPosition().getX() > 0);
-    N_ASSERT(p2->getPosition().getX() < 100);
-}
-
-int main() {
-    TEST_HEADER;
-    RUN_TEST(test_simple_two_body_movement);
-    TEST_FOOTER;
-    return (failed_tests == 0) ? 0 : 1;
+    REQUIRE(p1->getPosition().getX() > 0);
+    REQUIRE(p2->getPosition().getX() < 100);
 }
