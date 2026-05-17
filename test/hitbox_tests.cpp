a7ae79a36aa1e4f853ee478a99c933155143f1dd
Oleksandr K. <73932270+kotenko-o@users.noreply.github.com>
2026-05-17 15:36:12 +0200

    test: optimise Catch2 building

--- test/hitbox_tests.cpp
+++ test/hitbox_tests.cpp
@@ -1,7 +1,7 @@
-#include <catch2/catch_test_macros.hpp>
 #include <objects.hpp>
 #include <hitbox.hpp>
 #include <simple_vector.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include <physics.hpp>
 #include <cmath>
 #include <memory>
